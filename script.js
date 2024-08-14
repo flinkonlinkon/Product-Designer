c2bd5eea5c9638d384fe0a2ad239770ab1d579dc
flinkon9 <flinkon9@gmaile.com>
2024-08-14 17:14:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu')
+let show = document.querySelector('#show')
+
+menu.addEventListener('click',()=>{
+    show.classList.toggle('hidden')
+    show.classList.toggle('block')
+    // console.log("hello");
+    
+})
